353e1e586433cf99eeb32e90ea4f895e2a27caa7
Anard <anard@free.fr>
2019-09-23 12:30:50 +0200

    Add files via upload

--- Package.swift
+++ Package.swift
@@ -1,19 +1,26 @@
-// swift-tools-version:4.2
+// swift-tools-version:4.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Silica",
+    products: [
+    	.library(
+    		name: "Silica",
+    		targets: ["Silica"]
+    	)
+    ],
     dependencies: [
         .package(
-            url: "https://github.com/PureSwift/Cairo.git", 
+            url: "../Cairo", 
             .branch("master")
         )
     ],
     targets: [
         .target(
-            name: "Silica"
+            name: "Silica",
+            dependencies: ["Cairo"]
         )
     ]
 )
